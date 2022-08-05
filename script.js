dc79c2956b1f74aef2c41d3805bc4abc4b696f90
Aakash <vermaaakash1000@gmail.com>
2022-08-05 18:05:05 +0530

    ethers object imported

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+import { ethers } from "./ethers-5.6.esm.min.js";
+
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+connectButton.onclick = connect;
+
 async function connect() {
+  console.log("its working");
   if (typeof window.ethereum !== "undefined") {
     await window.ethereum.request({ method: "eth_requestAccounts" });
     console.log("connected");
-    document.getElementById("connectButton").innerHTML = "Connected";
+    connectButton.innerHTML = "Connected";
   } else {
-    document.getElementById("connectButton").innerHTML =
-      "Please connect to Metamask";
+    connectButton.innerHTML = "Please connect to Metamask";
   }
 }
